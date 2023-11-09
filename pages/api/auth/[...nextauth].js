836566a3a938a0d3be706131f2ba5b3227a68069
tuongvyluan <luantuongvy13@gmail.com>
2023-11-09 13:57:31 +0700

    Config secret env to deploy

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -84,6 +84,7 @@ const authOptions = {
 		error: '/auth/error', // Error code passed in query string as ?error=
 		verifyRequest: '/auth/verify-request', // (used for check email message)
 		newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-	}
+	},
+	secret: process.env.NEXT_PUBLIC_SECRET
 };
 export default NextAuth(authOptions);
