a6a1ea8113954dc0c53d9cdda34599e3655a4005
nhoxtruong002 <shikikanShaw@gmail.com>
2023-12-30 20:59:23 +0700

    Dashboard booking

--- layout/Studio/Index.js
+++ layout/Studio/Index.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Avatar, Card, CardBody, Loading, WidgetStatCard } from '../../ui';
 import { Users } from 'icons/solid';
-import { fetcher } from 'lib';
+import { fetcher, formatMonthYear, formatPrice } from 'lib';
 import { BASE_URL, SOCIAL_PAGE } from 'lib/env';
 import Link from 'next/link';
+import ChartComponent from 'components/ChartComponent';
+import { sharedOptions, gridOptions, colors } from 'lib/chartHelper';
 
 function StudioIndexPage({ studioId }) {
 	const [studio, setStudio] = useState({
@@ -21,8 +23,15 @@ function StudioIndexPage({ studioId }) {
 		artists: [],
 		bookings: []
 	});
+	const [bookingStat, setBookingStat] = useState([]);
+	const [revenueStat, setRevenueStat] = useState([]);
+	const [tattooStat, setTattooStat] = useState([]);
+
+	const [bookingLabels, setBookingLabels] = useState([]);
+	const [bookingDataSet, setBookingDataSet] = useState([]);
 
 	if (studioId && !studio.id) {
+		// Get studio details
 		fetcher(`${BASE_URL}/studios/studio-details?id=${studioId}`).then((response) => {
 			setStudio({
 				...studio,
@@ -37,6 +46,38 @@ function StudioIndexPage({ studioId }) {
 				bookings: response.bookings
 			});
 		});
+
+		fetcher(`${BASE_URL}/dashboard/booking?studioId=${studioId}`).then(
+			(response) => {
+				setBookingStat(response.sta);
+				setBookingDataSet([
+					{
+						label: 'Tổng số đơn hàng',
+						...colors[0],
+						data: response.sta.map((s) => s.noOfBooking)
+					},
+					{
+						label: 'Tổng số đơn hàng đã hoàn thành',
+						...colors[1],
+						data: response.sta.map((s) => s.noOfBookingDone)
+					}
+				]);
+				setBookingLabels(response.sta.map((s) => formatMonthYear(s.month)));
+			}
+		);
+
+		fetcher(`${BASE_URL}/dashboard/tattoo-studio?studioId=${studioId}`).then(
+			(response) => {
+				setTattooStat(response.sta);
+			}
+		);
+
+		fetcher(`${BASE_URL}/dashboard/revenue?studioId=${studioId}`).then(
+			(response) => {
+				setRevenueStat(response.sta);
+			}
+		);
+
 		return (
 			<div className="flex items-center justify-center h-full">
 				<Loading />
@@ -48,9 +89,11 @@ function StudioIndexPage({ studioId }) {
 				<div className="flex flex-wrap -mx-2 pb-2 md:gap-0">
 					<div className="w-full md:w-2/4 lg:w-1/4 px-2 pb-4">
 						<WidgetStatCard
-							title="Tổng đơn hàng"
+							title="Số hình xăm tháng này"
 							className="h-full"
-							value={studio.bookings?.length}
+							value={
+								tattooStat?.at(11)?.noOfTattoo ? tattooStat?.at(11)?.noOfTattoo : 0
+							}
 							icon={<Users width={16} height={16} />}
 							type={'blue'}
 						/>
@@ -59,7 +102,11 @@ function StudioIndexPage({ studioId }) {
 						<WidgetStatCard
 							title="Đơn hàng tháng này"
 							className="h-full"
-							value={'23'}
+							value={
+								bookingStat?.at(11)?.noOfBooking
+									? bookingStat?.at(11)?.noOfBooking
+									: 0
+							}
 							icon={<Users width={16} height={16} />}
 							type={'gray'}
 						/>
@@ -68,7 +115,7 @@ function StudioIndexPage({ studioId }) {
 						<WidgetStatCard
 							title={'Doanh thu tháng này'}
 							className="h-full"
-							value={'23,465,563'}
+							value={formatPrice(revenueStat?.at(11)?.revenue)}
 							icon={<Users width={16} height={16} />}
 							type={'indigo'}
 						/>
@@ -83,53 +130,74 @@ function StudioIndexPage({ studioId }) {
 						/>
 					</div>
 				</div>
-				{studio.artists && studio.artists?.length > 0 ? (
-					<div>
-						<Card>
-							<CardBody className="flex flex-wrap justify-center gap-3">
-								{studio.artists.map((artist, artistIndex) => (
-									<div key={artist.id} className="min-w-max w-1/4 px-2 mb-3">
-										<div className="w-full block text-gray-900 dark:text-white">
-											<div className="flex justify-center">
-												<Link
-													target="_blank"
-													href={`${SOCIAL_PAGE}/artist/${artist.artist.id}`}
-												>
-													<Avatar
-														size={48}
-														src={
-															artist.artist.avatar
-																? artist.artist.avatar
-																: '/images/ATL.png'
-														}
-														alt={artist.artist.fullName}
-														className={'cursor-pointer'}
-													/>
-												</Link>
-											</div>
-											<div className="mt-1 flex justify-center text-center">
-												<div>
-													<span className="block">{artist.artist.fullName}</span>
+				{
+					// Artists
+				}
+				<div>
+					{studio.artists && studio.artists?.length > 0 ? (
+						<div>
+							<Card>
+								<CardBody className="flex flex-wrap justify-center gap-3">
+									{studio.artists.map((artist, artistIndex) => (
+										<div key={artist.id} className="min-w-max w-1/4 px-2 mb-3">
+											<div className="w-full block text-gray-900 dark:text-white">
+												<div className="flex justify-center">
+													<Link
+														target="_blank"
+														href={`${SOCIAL_PAGE}/artist/${artist.artist.id}`}
+													>
+														<Avatar
+															size={48}
+															src={
+																artist.artist.avatar
+																	? artist.artist.avatar
+																	: '/images/ATL.png'
+															}
+															alt={artist.artist.fullName}
+															className={'cursor-pointer'}
+														/>
+													</Link>
+												</div>
+												<div className="mt-1 flex justify-center text-center">
+													<div>
+														<span className="block">{artist.artist.fullName}</span>
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
-								))}
-							</CardBody>
-						</Card>
-					</div>
-				) : (
-					<div className="flex items-center justify-center gap-1 text-lg">
-						<div className="text-center">
-							<span>Bạn đang không hợp tác với nghệ sĩ nào, vào </span>
-							<Link href={'/artist'}>đây</Link>
-							<span>
-								{' '}
-								để nhập key từ nghệ sĩ và thêm họ vào tiệm xăm của mình nhé.
-							</span>
+									))}
+								</CardBody>
+							</Card>
 						</div>
+					) : (
+						<div className="flex items-center justify-center gap-1 text-lg">
+							<div className="text-center">
+								<span>Bạn đang không hợp tác với nghệ sĩ nào, vào </span>
+								<Link href={'/artist'}>đây</Link>
+								<span>
+									{' '}
+									để nhập key từ nghệ sĩ và thêm họ vào tiệm xăm của mình nhé.
+								</span>
+							</div>
+						</div>
+					)}
+				</div>
+				{
+					// Charts
+				}
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+					<div>
+						<ChartComponent
+							type={'bar'}
+							title="Doanh thu 12 tháng vừa qua"
+							options={{
+								...sharedOptions,
+								...gridOptions
+							}}
+							data={{ labels: bookingLabels, datasets: bookingDataSet }}
+						/>
 					</div>
-				)}
+				</div>
 			</>
 		);
 }
